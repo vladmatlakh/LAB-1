553bdf7e2f4f695c6b1dd1131c059a8d3061c714
vladmatlakh <vlad.matlakh1@gmail.com>
2025-09-02 18:00:12 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,21 @@
+number = int(input('WRITE NUMBER: '))
+float_number = 1.23
+text_str = 'Hello world'
+d = bool
+f = [1,2,3,4,5,6]
+g = (1,2,3,4,5,6)
+text = "hello world"
+h = set(text)
+
+k = {'1': 'computer', '2': 'programming'}
+
+if a < 5:
+    d = True
+    print(d)
+elif a >5:
+    d = False
+    print(d)
+
+print("NUMBERS: ", a,"/", b,"/", f[2],"/", g[0])
+print("TEXT: ", c,"/", list(h)[0], "/", k['1'])
+print("CLASSES: ", type(a), type(b), type(c), type(d), type(f), type(g), type(h), type(k))
