f4097a921657be6b3ed6d5c3d7fbd4eaf28795c2
vladmatlakh <vlad.matlakh1@gmail.com>
2025-09-03 08:50:11 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 number = int(input('WRITE NUMBER: '))
 float_number = 1.23
+float_number2 = 23.563
 text_str = 'Hello world'
+text_str2 = 'program'
 bool_variable = bool
 number_list = [1,2,3,4,5,6]
 tuple_list = (1,2,3,4,5,6)
@@ -15,7 +17,7 @@ elif number >5:
     bool_variable = False
     print(bool_variable)
 
-print("NUMBERS: ", number,"/ ", float_number,"/ ", number_list[2],"/ ", tuple_list[0])
-print("TEXT: ", text_str,"/ ", list(set_list)[0], "/ ", vocabulary['1'])
-print("CLASSES: ", type(number), type(float_number), type(text_str), type(bool_variable),
+print("NUMBERS: ", number,"/ ", float_number,"/ ",float_number2,'/ ' , number_list[2],"/ ", tuple_list[0])
+print("TEXT: ", text_str,"/ ",text_str2,'/ ', list(set_list)[0], "/ ", vocabulary['1'])
+print("CLASSES: ", type(number), type(float_number),type(float_number2), type(text_str),type(text_str2), type(bool_variable),
       type(set_list), type(tuple_list), type(number_list), type(vocabulary))
